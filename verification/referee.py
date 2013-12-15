80af8847547c61d9448f18bd26f12c585ab2e7c1
Valentin Bryukhanov <bvv.mag@gmail.com>
2013-12-15 16:19:04 +1200

    referee

--- verification/referee.py
+++ verification/referee.py
@@ -1,31 +1,3 @@
-"""
-CheckiOReferee is a base referee for checking you code.
-    arguments:
-        tests -- the dict contains tests in the specific structure.
-            You can find an example in tests.py.
-        cover_code -- is a wrapper for the user function and additional operations before give data
-            in the user function. You can use some predefined codes from checkio.referee.cover_codes
-        checker -- is replacement for the default checking of an user function result. If given, then
-            instead simple "==" will be using the checker function which return tuple with result
-            (false or true) and some additional info (some message).
-            You can use some predefined codes from checkio.referee.checkers
-        add_allowed_modules -- additional module which will be allowed for your task.
-        add_close_builtins -- some closed builtin words, as example, if you want, you can close "eval"
-        remove_allowed_modules -- close standard library modules, as example "math"
-
-checkio.referee.checkers
-    checkers.float_comparison -- Checking function fabric for check result with float numbers.
-        Syntax: checkers.float_comparison(digits) -- where "digits" is a quantity of significant
-            digits after coma.
-
-checkio.referee.cover_codes
-    cover_codes.unwrap_args -- Your "input" from test can be given as a list. if you want unwrap this
-        before user function calling, then using this function. For example: if your test's input
-        is [2, 2] and you use this cover_code, then user function will be called as checkio(2, 2)
-    cover_codes.unwrap_kwargs -- the same as unwrap_kwargs, but unwrap dict.
-
-"""
-
 from checkio.signals import ON_CONNECT
 from checkio import api
 from checkio.referees.io import CheckiOReferee
@@ -34,16 +6,31 @@ from checkio.referees import checkers
 
 from tests import TESTS
 
+
+def count_diff(first, second):
+    return len(str(first)) - sum([f == s for f, s in zip(str(first), str(second))])
+
+
+def checker(right_answer, user_answer):
+    answer, numbers = right_answer
+    if not isinstance(user_answer, (list)):
+        return False, (False, "The result must be a list.")
+    if any(not isinstance(el, int) or el <= 0 for el in user_answer):
+        return False, (False, "The result must be a list with positive numbers.")
+    if any(el not in numbers for el in user_answer):
+        return False, (True, "There is some number not from the given list.")
+    if len(user_answer) > len(answer):
+        return False, (True, "The chain can be shorter.")
+    if len(user_answer) < 2 or user_answer[0] != numbers[0] or user_answer[-1] != numbers[-1]:
+        return False, (True, "The edges of chain are wrong.")
+    for i, el in enumerate(user_answer[:-1]):
+        if count_diff(user_answer[i], user_answer[i + 1]) != 1:
+            return False, (True, "The wrong step {0} - {1}.".format(i, i + 1))
+    return True, (True, "All right!")
+
+
 api.add_listener(
     ON_CONNECT,
     CheckiOReferee(
         tests=TESTS,
-        cover_code={
-            'python-27': cover_codes.unwrap_args,  # or None
-            'python-3': cover_codes.unwrap_args
-        },
-        # checker=None,  # checkers.float.comparison(2)
-        # add_allowed_modules=[],
-        # add_close_builtins=[],
-        # remove_allowed_modules=[]
-    ).on_ready)
+        checker=checker).on_ready)
